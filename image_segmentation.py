d9875d5baba198f12adde6a0ca82da3693ff5dd9
ngkel <ngkel@connect.hku.hk>
2019-09-18 21:30:15 +0800

    Update image_segmentation.py

--- image_segmentation.py
+++ image_segmentation.py
@@ -1,90 +1,11 @@
-import os
-import glob
-import zipfile
-import functools
+from __future__ import absolute_import, division, print_function, unicode_literals
 
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib as mpl
-mpl.rcParams['axes.grid'] = False
-mpl.rcParams['figure.figsize'] = (12,12)
 
-from sklearn.model_selection import train_test_split
-import matplotlib.image as mpimg
-import pandas as pd
-from PIL import Image
-
-import tensorflow as tf
-import tensorflow.contrib as tfcontrib
-from tensorflow.python.keras import layers
-from tensorflow.python.keras import losses
-from tensorflow.python.keras import models
-from tensorflow.python.keras import backend as K  
-
-import os
-
-# Upload the API token.
-def get_kaggle_credentials():
-  token_dir = os.path.join(os.path.expanduser("~"),".kaggle")
-  token_file = os.path.join(token_dir, "kaggle.json")
-  if not os.path.isdir(token_dir):
-    os.mkdir(token_dir)
-  try:
-    with open(token_file,'r') as f:
-      pass
-  except IOError as no_file:
-    try:
-      from google.colab import files
-    except ImportError:
-      raise no_file
-    
-    uploaded = files.upload()
-    
-    if "kaggle.json" not in uploaded:
-      raise ValueError("You need an API key! see: "
-                       "https://github.com/Kaggle/kaggle-api#api-credentials")
-    with open(token_file, "wb") as f:
-      f.write(uploaded["kaggle.json"])
-    os.chmod(token_file, 600)
-
-get_kaggle_credentials()
-
-import kaggle
-
-competition_name = 'carvana-image-masking-challenge'
-
-# Download data from Kaggle and unzip the files of interest. 
-def load_data_from_zip(competition, file):
-  with zipfile.ZipFile(os.path.join(competition, file), "r") as zip_ref:
-    unzipped_file = zip_ref.namelist()[0]
-    zip_ref.extractall(competition)
-
-def get_data(competition):
-    kaggle.api.competition_download_files(competition, competition)
-    load_data_from_zip(competition, 'train.zip')
-    load_data_from_zip(competition, 'train_masks.zip')
-    load_data_from_zip(competition, 'train_masks.csv.zip')
-
-get_data(competition_name)
-
-img_dir = os.path.join(competition_name, "train")
-label_dir = os.path.join(competition_name, "train_masks")
-
-df_train = pd.read_csv(os.path.join(competition_name, 'train_masks.csv'))
-ids_train = df_train['img'].map(lambda s: s.split('.')[0])
-
-x_train_filenames = []
-y_train_filenames = []
-for img_id in ids_train:
-  x_train_filenames.append(os.path.join(img_dir, "{}.jpg".format(img_id)))
-  y_train_filenames.append(os.path.join(label_dir, "{}_mask.gif".format(img_id)))
-
-x_train_filenames, x_val_filenames, y_train_filenames, y_val_filenames = \
-                    train_test_split(x_train_filenames, y_train_filenames, test_size=0.2, random_state=42)
-
-num_train_examples = len(x_train_filenames)
-num_val_examples = len(x_val_filenames)
-
-print("Number of training examples: {}".format(num_train_examples))
-print("Number of validation examples: {}".format(num_val_examples))
-
+print(tf.__version__)
